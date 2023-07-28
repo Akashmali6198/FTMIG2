cad0cea773cb01446e5b96923affc63967ec593c
Akash Mali <maliakash6198@gmail.com>
2023-07-28 15:10:01 +0530

    banner updated

--- Magazine.php
+++ Magazine.php
@@ -1,7 +1,37 @@
 <?php include 'header.php' ?>
 <!-- header above -->
 
+<style>
+  /* banner style */
 
+.banner {
+  position: relative;
+  text-align: center;
+  height: 500px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 100px;
+  background: url('assets/images/conference.jpg') no-repeat center center;
+  background-size: cover;
+}
+
+.banner h1 {
+  color: #fff;
+  font-size: 4rem;
+  font-weight: bold;
+  margin: 0;
+  /* position: relative; */
+  text-align: center;
+  z-index: 1;
+}
+</style>
+<section>
+  <div class="container-fluid banner overlay">
+    <h1>Magazine</h1>
+  </div>
+
+</section>
 
 <style>
 
